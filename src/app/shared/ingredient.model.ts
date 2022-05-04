a6e0353e2047e763fd4aeb7bde1183c5f260c1f0
Telvin Mathews <tmathews@appddictionstudio.com>
2022-05-04 14:31:35 -0500

    created and output a shopping list.

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,11 +1,4 @@
 export class Ingredient {
-  public name: string;
-  public amount: number;
-
-
-  constructor(name: string, amount: number) {
-    this.name = name
-    this.amount = amount
+  constructor(public name: string, public amount: number) {
   }
-
 }
