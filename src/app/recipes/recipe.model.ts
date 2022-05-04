8706587d7119452fb0a20bcd7f4c1b965bbc68ca
Telvin Mathews <tmathews@appddictionstudio.com>
2022-05-04 09:46:20 -0500

    added recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+
+	constructor(name: string, desc: string, image: string) {
+    this.name = name;
+    this.imagePath = this.imagePath;
+    this.description = this.description;
+	}
+
+}
