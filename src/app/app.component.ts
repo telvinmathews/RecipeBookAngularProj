1c059652e9a396ca672a6c8f8558d036aa70362c
telvinmathews <telvinmathews@gmail.com>
2022-05-06 18:28:42 -0500

    added focus to the name input element using viewchild decorator in the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Recipe Book';
   imgUrl = 'https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_1280.jpg';
   count = 0;
   name: string;
   username: string;
   private _customerName: string;
+  @ViewChild('nameRef') nameElementRef: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.nameElementRef.nativeElement.focus();
+    console.log(this.nameElementRef);
+  }
 
   get customerName(): string {
     return this._customerName;
